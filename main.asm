8ae6ccca929323083a069da426c3235e5d49a95a
kingtuler <kingtuler@gmail.com>
2023-09-23 10:15:24 +0400

    fill array

--- main.asm
+++ main.asm
@@ -2,17 +2,36 @@
 %include "io64.inc"
 
 section .bss
-    array resd 100
-    array_len resd 1
+    arr resd 110
+    arr_size resd 1
 
 section .text
     global main
     main:
-    mov rbp, rsp; for correct debugging
         PRINT_STRING "Array length: "
-        PRINT_DEC 4, [array_len]
+        GET_DEC 4, [arr_size]
+        PRINT_DEC 4, [arr_size]
         NEWLINE
-  
+        
+    fill:
+        PRINT_STRING "--Array fill--"
+        NEWLINE
+        mov ecx, [arr_size]
+        mov ebx, arr  ; загружаем адрес начала массива в ebx
 
-        PRINT_STRING "Array fill:"
+        mov ecx, [arr_size]  ; загружаем размер массива в ecx
+            mov ebx, arr  ; загружаем адрес начала массива в ebx
+        .fill_loop:
+            cmp ecx, 0  ; проверяем, если ecx равен 0, то выходим из цикла
+            je .end_loop
+            PRINT_STRING "elem of array:"
+            GET_DEC 4, [ebx]  ; считываем значение и сохраняем в текущей ячейке массива
+            PRINT_DEC 4, [ebx]
+            NEWLINE
+            add ebx, 4  ; переходим к следующей ячейке
+            sub ecx, 1  ; уменьшаем счетчик цикла на 1
+            jmp .fill_loop  ; переходим в начало цикла
+        .end_loop:
+            PRINT_STRING "--Array filled--"
 
+    

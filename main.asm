55d32f200ea3132f968126601a7e05651f117abb
kingtuler <kingtuler@gmail.com>
2023-09-30 23:41:27 +0400

    писанина жесткая

--- main.asm
+++ main.asm
@@ -1,13 +1,21 @@
 ; библиотека для макросов SASM
-%include "io64.inc"
+%include "io64.inc"  
 
-section .bss
-    arr resd 110
-    arr_size resd 1
+section .bss ;для объявления неинициализированных переменных
+    arr: resd 110   ;4-байтных слов
+    arr_size: resd 1  ;4-байтных слов
+    crc_string: resb 256 ; указывает количество байт, которые необходимо зарезервировать
+    string_size: resd 256
+
+    src_result:resb 256
+section .data:
+    src_result_size: db 256
+
 
 section .text
     global main
     main:
+    mov rbp, rsp; for correct debugging
         PRINT_STRING "Array length: "
         GET_DEC 4, [arr_size]
         PRINT_DEC 4, [arr_size]
@@ -26,6 +34,7 @@ section .text
             je .end_loop
             PRINT_STRING "elem of array:"
             GET_DEC 4, [ebx]  ; считываем значение и сохраняем в текущей ячейке массива
+            GET_CHAR al ; <----------
             PRINT_DEC 4, [ebx]
             NEWLINE
             add ebx, 4  ; переходим к следующей ячейке
@@ -33,5 +42,28 @@ section .text
             jmp .fill_loop  ; переходим в начало цикла
         .end_loop:
             PRINT_STRING "--Array filled--"
+            NEWLINE
 
+    CRC_fill_arr:
+        PRINT_STRING "Enter the string for CRC32: "
+        GET_STRING [crc_string], 256
+        PRINT_STRING [crc_string]
+        NEWLINE       
+        ret
+
+        mov eax, 0xFFFFFFFF ; src=0xFFFFFFFF
+        mov ecx,256
+
+        .out_loop:
+            cmp ecx,0
+            je .end_loop
+            mov ebx, 8
+            .inner_loop:
+                cmp ebx, 0   ; проверяем, достигли ли мы конца внутреннего цикла
+                jne .inner_loop
+
+
+            
+        
+        
     
--- tmp.cpp
+++ tmp.cpp
@@ -21,37 +21,34 @@ int main(){
 
 }
 
-#include <stddef.h>
-#include <stdint.h>
-/*
-  Name  : CRC-32
-  Poly  : 0x04C11DB7    x^32 + x^26 + x^23 + x^22 + x^16 + x^12 + x^11
-                       + x^10 + x^8 + x^7 + x^5 + x^4 + x^2 + x + 1
-  Init  : 0xFFFFFFFF
-  Revert: true
-  XorOut: 0xFFFFFFFF
-  Check : 0xCBF43926 ("123456789")
-  MaxLen: 268 435 455 байт (2 147 483 647 бит) - обнаружение
-   одинарных, двойных, пакетных и всех нечетных ошибок
-*/
-uint_least32_t Crc32(unsigned char *buf, size_t len)
-{
-    uint_least32_t crc_table[256];
-    uint_least32_t crc; int i, j;
+#include <stdint.h> // заголовочный файл для использования типов данных с фиксированной длиной
+#include <stdlib.h> // заголовочный файл для использования функции malloc
 
-    for (i = 0; i < 256; i++)
-    {
-        crc = i;
-        for (j = 0; j < 8; j++)
-            crc = crc & 1 ? (crc >> 1) ^ 0xEDB88320UL : crc >> 1;
+uint32_t crc32(const void *data, size_t length) { // объявление функции crc32, которая принимает указатель на данные и длину буфера данных
+    static uint32_t crc_table[256]; // определение статического массива crc_table размером 256 элементов типа uint32_t
+    static int init = 0; // объявление статической переменной init типа int и инициализация ее значением 0
+    uint32_t crc = 0xFFFFFFFFUL; // объявление переменной crc типа uint32_t и инициализация ее значением 0xFFFFFFFFUL
 
-        crc_table[i] = crc;
-    };
+    if (!init) { // проверка, была ли уже выполнена инициализация crc_table
+        for (int i = 0; i < 256; i++) { // цикл for, который проходит через каждый элемент массива crc_table
+            uint32_t c = i; // объявление переменной c типа uint32_t и инициализация ее значением i
+            for (int j = 0; j < 8; j++) { // вложенный цикл for, который проходит через каждый бит переменной c
+                if (c & 1) { // проверка, является ли младший бит переменной c равным 1
+                    c = 0xEDB88320L ^ (c >> 1); // изменение значения переменной c по формуле CRC-32
+                } else {
+                    c = c >> 1; // сдвиг значения переменной c на один бит вправо
+                }
+            }
+            crc_table[i] = c; // присвоение элементу массива crc_table[i] значения переменной c
+        }
+        init = 1; // установка значения переменной init в 1 для обозначения выполнения инициализации crc_table
+    }
 
-    crc = 0xFFFFFFFFUL;
+    const uint8_t *bytes = (const uint8_t *) data; // объявление указателя bytes на данные типа uint8_t и приведение указателя data к типу const uint8_t *
+    for (size_t i = 0; i < length; i++) { // цикл for, который проходит через каждый байт в буфере данных
+        crc = crc_table[(crc ^ bytes[i]) & 0xFF] ^ (crc >> 8); // изменение значения переменной crc по формуле CRC-32
+    }
 
-    while (len--)
-        crc = crc_table[(crc ^ *buf++) & 0xFF] ^ (crc >> 8);
-
-    return crc ^ 0xFFFFFFFFUL;
+    return crc ^ 0xFFFFFFFFUL; // возврат итогового значения crc, которое является контрольной суммой для данного буфера данных
 }
+
